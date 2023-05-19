4413433a883dbb1aaf5a590bb3afe287b5134c23
kongmenglor <kongmeng.k.lor@gmail.com>
2023-05-19 09:46:22 -0600

    Working on menu display and navigation.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { ValueServiceService } from './value-service.service';
 import { HttpBackend, HttpClient } from '@angular/common/http';
-
+import { Router,NavigationEnd, Event as NavigationEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +15,26 @@ export class AppComponent {
   
   public isSignedin: boolean;
   public login: LoginComponent;
+  public isMenuPage : boolean = false;
 
-  constructor(){
+  event
+
+  constructor(private router:Router){
     this.isSignedin = false;
     //this.login = new LoginComponent;
     
+    this.isMenuPage = true;
+
+    this.event = this.router.events.subscribe((event : NavigationEvent) => {
+      if(event instanceof NavigationEnd){
+        console.log(this.router.url);
+        if(this.router.url === "/login" || this.router.url === "/signup" || this.router.url === ""){
+          this.isMenuPage= false;
+        }
+      }
+    });
+
+
   }
 
 }
