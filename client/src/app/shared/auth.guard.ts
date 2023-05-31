6ccbc4839df2f2d1fab354a138e0d42f8429a776
dayyoung6165 <dayyoung6165@penguin>
2023-05-30 21:55:58 -0600

    authentication works

--- client/src/app/shared/auth.guard.ts
+++ client/src/app/shared/auth.guard.ts
@@ -1,5 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { of, Observable, firstValueFrom } from 'rxjs';
+import { AuthService } from './auth.service';
 import { CanActivateFn } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+      if (localStorage.getItem('email')) {
+            return true;
+      }
+      this.router.navigate(['/login']);
+      return false;
+  }
+}
