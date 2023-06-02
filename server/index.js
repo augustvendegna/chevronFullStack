ac98e3f378114ee2a030a4669d7fa577e257fd7d
kongmenglor <kongmeng.k.lor@gmail.com>
2023-06-02 11:53:21 -0600

    Got leaderboard to display according to public and private parameters.

--- server/index.js
+++ server/index.js
@@ -151,7 +151,7 @@ app.post("/changePassword", async (req, res) => {
 });
 
 app.get("/getLeaderboardInfo", async (req, res) => {
-  const values = await pgClient.query("SELECT u.username, MAX(s.score) FROM users u, submissions s WHERE u.user_id = s.user_id AND challenge_id = $1 GROUP BY u.username ORDER BY MAX(s.score) ASC", [req.query.CID])
+  const values = await pgClient.query("SELECT username, MAX(score) FROM users JOIN submissions ON users.user_id = submissions.user_id WHERE challenge_id = 1 AND is_public = true OR submissions.user_id = 4 GROUP BY username ORDER BY MAX(score) ASC", [req.query.CID, req.query.UID])
   res.send(values.rows);
   console.log(values.rows);
 });
