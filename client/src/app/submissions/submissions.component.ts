01b49d528bc2b6bd06f925ac9bdff1eeafd6b26a
dayyoung6165 <dayyoung6165@penguin>
2023-06-08 11:00:41 -0600

    date logic

--- client/src/app/submissions/submissions.component.ts
+++ client/src/app/submissions/submissions.component.ts
@@ -40,79 +40,99 @@ export class SubmissionsComponent {
         localStorage.setItem('CID', this.challenge_id.toString());
         //var dates: Number [];
 
-        var valid = true;
-
-        // Upload Service: Check User Submissions
-        this.uploadService.checkUserSubmissions(parseInt(localStorage.getItem('UID')), this.challenge_id).subscribe((data: Object[]) => {
-          if (data['status'] == '0') {
-            var submissionsLeft = data['submissions_left'] - 1;
-            alert('submissions left: ' + submissionsLeft); 
-          
-            // Upload Service: Get Dates
-            this.uploadService.getDates(localStorage.getItem('CID')).subscribe((data: Object []) => {
-              response = data;
-              var resp = JSON.stringify(response[0]);
-              resp = resp.replaceAll("\":\"", ",");
-              resp = resp.replaceAll(".", ",");
-              resp = resp.replaceAll("-", "/");
-              resp = resp.replaceAll("T", " ");
-              resp = resp.replaceAll("}", ",");
-              var splitResp = resp.split(",");
-              console.log(splitResp)
-              var public_end_date = new Date(splitResp[1]).getTime();
-              console.log(public_end_date);
-              var private_end_date = new Date(splitResp[4]).getTime();
-              console.log(private_end_date);
-              const now = new Date().getTime();
-              console.log(now);
-              if(now < public_end_date) {
-                this.is_public = true;
-              } else if(now > public_end_date && now < private_end_date) {
-                this.is_public = false;
-              }
-              //dates.push(public_end_date);
-              //dates.push(private_end_date);
-              console.log(this.is_public);
-
-              // Upload Service: Sent Info
-              this.uploadService.sentInfo(this.fileName, this.challenge_id, this.is_public, parseInt(localStorage.getItem('UID')), this.score).subscribe(resp => {
-                alert("sent");
-
-                // Upload Service: Get Submission ID
-                this.uploadService.getSubmissionID(parseInt(localStorage.getItem('UID'))).subscribe((data: Object[]) => {
-                  //var valid = true;
+        const compareNow = new Date();
+        const timestamp = compareNow.getTime();
+        var publicSD = new Date(localStorage.getItem("public_start_date")).getTime();
+        var privateED = new Date(localStorage.getItem("private_end_date")).getTime();
+        var publicED = new Date(localStorage.getItem("public_end_date")).getTime();
+        var privateSD = new Date(localStorage.getItem("private_start_date")).getTime();
+        if ((publicSD < timestamp) && (timestamp < privateED)) {
+          if ((timestamp > publicED) && (timestamp < privateSD)) {
+            // date > public end date
+            // date < less than private start date 
+            alert('outside of bounds - private and public')
+          } else {
+            var valid = true;
+            // Upload Service: Check User Submissions
+            this.uploadService.checkUserSubmissions(parseInt(localStorage.getItem('UID')), this.challenge_id).subscribe((data: Object[]) => {
+              if (data['status'] == '0') {
+                var submissionsLeft = data['submissions_left'] - 1;
+                alert('submissions left: ' + submissionsLeft); 
+              
+                // Upload Service: Get Dates
+                this.uploadService.getDates(localStorage.getItem('CID')).subscribe((data: Object []) => {
                   response = data;
                   var resp = JSON.stringify(response[0]);
-                  resp = resp.replaceAll(":", ",");
+                  resp = resp.replaceAll("\":\"", ",");
+                  resp = resp.replaceAll(".", ",");
+                  resp = resp.replaceAll("-", "/");
+                  resp = resp.replaceAll("T", " ");
                   resp = resp.replaceAll("}", ",");
                   var splitResp = resp.split(",");
-                  console.log('user_id: ' + localStorage.getItem('UID'));
-                  console.log('c_id: ' + this.challenge_id);
+                  console.log(splitResp)
+                  var public_end_date = new Date(splitResp[1]).getTime();
+                  console.log(public_end_date);
+                  var private_end_date = new Date(splitResp[4]).getTime();
+                  console.log(private_end_date);
+                  const now = new Date().getTime();
+                  console.log(now);
+                  if(now < public_end_date) {
+                    this.is_public = true;
+                  } else if(now > public_end_date && now < private_end_date) {
+                    this.is_public = false;
+                  }
+                  //dates.push(public_end_date);
+                  //dates.push(private_end_date);
+                  console.log(this.is_public);
 
-                  // Upload Service: Add Submission
-                  this.uploadService.addSubmission(this.selectedFile, splitResp[1]).subscribe(resp => {
-                    alert("uploaded")
-                  });
+                  // Upload Service: Sent Info
+                  this.uploadService.sentInfo(this.fileName, this.challenge_id, this.is_public, parseInt(localStorage.getItem('UID')), this.score).subscribe(resp => {
+                    alert("sent");
 
-                  // Upload Service: Get Test Flag
-                  this.uploadService.getTestFlag(localStorage.getItem('CID')).subscribe((data: Object[]) => {
-                    testFlag = data;
-                    var resp2 = JSON.stringify(testFlag[0]);
-                    resp2 = resp2.replaceAll(":", ",");
-                    resp2 = resp2.replaceAll("}", ",");
-                    resp2 = resp2.replaceAll(/["']/g, ",");
-                    var splitResp2 = resp2.split(",");
-                    this.uploadService.computeScore(parseInt(splitResp[1]), splitResp2[4].toString()).subscribe(resp => {});
+                    // Upload Service: Get Submission ID
+                    this.uploadService.getSubmissionID(parseInt(localStorage.getItem('UID'))).subscribe((data: Object[]) => {
+                      //var valid = true;
+                      response = data;
+                      var resp = JSON.stringify(response[0]);
+                      resp = resp.replaceAll(":", ",");
+                      resp = resp.replaceAll("}", ",");
+                      var splitResp = resp.split(",");
+                      console.log('user_id: ' + localStorage.getItem('UID'));
+                      console.log('c_id: ' + this.challenge_id);
+
+                      // Upload Service: Add Submission
+                      this.uploadService.addSubmission(this.selectedFile, splitResp[1]).subscribe(resp => {
+                        alert("uploaded")
+                      });
+
+                      // Upload Service: Get Test Flag
+                      this.uploadService.getTestFlag(localStorage.getItem('CID')).subscribe((data: Object[]) => {
+                        testFlag = data;
+                        var resp2 = JSON.stringify(testFlag[0]);
+                        resp2 = resp2.replaceAll(":", ",");
+                        resp2 = resp2.replaceAll("}", ",");
+                        resp2 = resp2.replaceAll(/["']/g, ",");
+                        var splitResp2 = resp2.split(",");
+                        this.uploadService.computeScore(parseInt(splitResp[1]), splitResp2[4].toString()).subscribe(resp => {});
+                      });
+                    });
+                    alert("computed")
                   });
                 });
-                alert("computed")
-              });
+              } else {
+                valid = false;
+                alert('no more submissions for this challenge');
+              }
             });
-          } else {
-            valid = false;
-            alert('no more submissions for this challenge');
           }
-        });  
+           
+        } else {
+          console.log('public_start_date: ' + publicSD);
+          console.log('private_end_date: ' + privateED);
+          console.log('timestamp: ' + timestamp);
+          
+          alert('outside of bounds');
+        }
       }
     } else {
       alert("Please select a file first")
