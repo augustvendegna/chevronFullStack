88eb204b804e20b2c36cb08e7d354959b0cf9146
Gus Vendegna <augustvendegna@mines.edu>
2023-06-08 15:38:39 -0600

    Working dockerfile :)

--- runStack.sh
+++ runStack.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 cd /client
+
+su -c "pg_ctl start -D /var/lib/postgresql/data" postgres
 nohup lite-server --baseDir="/client/dist/website-one" 2>&1 >> /var/log/ng.log &
 node /server/index.js
